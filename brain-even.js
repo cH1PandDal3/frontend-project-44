09af9ad5d181ec3909734d2471cff59c8a58fc70
Rodion <fxckitmannn@gmail.com>
2025-12-12 15:43:40 +0700

    no mistakes

---
+++ brain-even.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import game from '../src/cli.js';
+import checkAnswer from '../src/checkAnswer.js';
+
+const isEvenGame = () => {
+  const name = game();
+  let sum = 0;
+  console.log('Answer "yes" if the number is even, otherwise answer "no"');
+  do {
+    const getRandomInt = () => Math.floor(Math.random() * 100) + 1;
+    const isEven = (num) => num % 2 === 0;
+    const getRandomNum = getRandomInt();
+
+    console.log(`Question: ${getRandomNum}`);
+
+    const correctAnswer = isEven(getRandomNum) ? 'yes' : 'no';
+    const firstAnswer = readlineSync.question('Your answer is: ');
+    if (checkAnswer(firstAnswer.toLowerCase(), correctAnswer, name)) {
+      sum += 1;
+    } else {
+      return;
+    }
+  } while (sum < 3);
+  if (sum === 3) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+isEvenGame();
---
+++ checkAnswer.js
@@ -0,0 +1,13 @@
+const checkAnswer = (answer, hiddenValue, name) => {
+  if (answer === hiddenValue) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(
+    `'${answer}' is wrong answer ;(. Correct answer was '${hiddenValue}'.`,
+  );
+  console.log(`Let's try again, ${name}!`);
+  return false;
+};
+
+export default checkAnswer;
